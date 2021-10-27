594878cd174a1d8aedf2c3050153c97c2c3f5d5e
VicenteAguileraPerez <vicente_prez@hotmail.com>
2021-10-26 23:31:47 -0500

    subiendo app

--- src/helpers/constantes.js
+++ src/helpers/constantes.js
@@ -2,7 +2,7 @@ const port = process.env.PORT||3000;
 const endpointMainPeople = "/api/v1/people";
 const DataBaseName = "people";
 const portMongo = 27017;
-const pathMongo = process.env.MONGO||`mongodb://localhost:${portMongo}/${DataBaseName}`
+const pathMongo = "mongodb+srv://people:personas@clustersistemasdistribu.ergvu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"//||`mongodb://localhost:${portMongo}/${DataBaseName}`
 const lengths =[150,100,300,100,1000,100,50,10];
 module.exports=
 {
